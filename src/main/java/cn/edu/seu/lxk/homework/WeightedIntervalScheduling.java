3a3cdaba042a4f052996175c2ae0118e89162d31
likai <zerlzerl@163.com>
2018-09-26 16:58:12 +0800

    区间问题/工作流调度问题作业

--- src/main/java/cn/edu/seu/lxk/homework/WeightedIntervalScheduling.java
+++ src/main/java/cn/edu/seu/lxk/homework/WeightedIntervalScheduling.java
@@ -5,30 +5,32 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
+ * 区间问题作业代码，区间问题也可称之为工作调度问题，在单一时间轴上，一个区间可表示一个工作，一个工作有起始时间，终止时间和价值
+ * 对区间最优的规划即对工作的最优优化，在一个时间段内可以达到最大的价值
  * Created by zerlz on 2018/9/19.
  */
 public class WeightedIntervalScheduling {
     private int[][] jobs;	//作业队列，每个作业都是 [id, startTime, finishTime, value]
-    private int[] memo;		//记忆数组
+    private int[] jobsSchedule;		//记忆数组
     private ArrayList<Integer> includedJobs = new ArrayList<>();
 
     public void calcSchedule(int[][] inputJobs){
         jobs= inputJobs;
-        memo = new int[jobs.length];
+        jobsSchedule = new int[jobs.length];
 
         Arrays.sort(jobs, Comparator.comparingInt(a -> a[2]));
 
-        memo[0]=0;
+        jobsSchedule[0]=0;
 
         for(int i = 1; i<jobs.length; i++){
-            memo[i] = Math.max( jobs[i][3]+memo[latestCompatible(i)],   memo[i-1] );
+            jobsSchedule[i] = Math.max( jobs[i][3]+ jobsSchedule[latestCompatible(i)],   jobsSchedule[i-1] );
         }
 
-        System.out.println("Memoization array: " + Arrays.toString(memo));
-        System.out.println("Maximum profit from the optimal set of jobs = " + memo[memo.length-1]);
+        System.out.println("记忆数组中的工作: " + Arrays.toString(jobsSchedule));
+        System.out.println("最右子问题的收益：" + jobsSchedule[jobsSchedule.length-1]);
 
-        findSolutionIterative(memo.length-1);
-        System.out.println("\nJobs Included in optimal solution:");
+        findSolutionRecursive(jobsSchedule.length-1);
+        System.out.println("\n最优结果:");
         for(int i=includedJobs.size()-1; i>=0; i--){
             System.out.println(getJobInfo(includedJobs.get(i)));
         }
@@ -56,7 +58,7 @@ public class WeightedIntervalScheduling {
     public void findSolutionIterative(int j){
         while (j>0){	//j==0停止
             int compatibleIndex = latestCompatible(j);	//找到与j兼容的最新完成工作
-            if(jobs[j][3]+ memo[compatibleIndex] > memo[j-1]){	//包含作业j的情况(最优子问题)
+            if(jobs[j][3]+ jobsSchedule[compatibleIndex] > jobsSchedule[j-1]){	//包含作业j的情况(最优子问题)
                 includedJobs.add(j);	//将当前工作的索引添加到数组
                 j=compatibleIndex;		//更新j
             }
@@ -73,7 +75,7 @@ public class WeightedIntervalScheduling {
         }
         else{
             int compatibleIndex = latestCompatible(j);	//找到与j兼容的最新完成工作
-            if(jobs[j][3]+ memo[compatibleIndex] > memo[j-1]){	//包含作业j的情况(最优子问题)
+            if(jobs[j][3]+ jobsSchedule[compatibleIndex] > jobsSchedule[j-1]){	//包含作业j的情况(最优子问题)
                 includedJobs.add(j);	//将当前工作的索引添加到数组
                 findSolutionRecursive(compatibleIndex);	//递归找到最新的兼容的工作
             }
